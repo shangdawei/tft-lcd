30bc37f11b104df46c5a8f8941695676610d2f7a
gxuliang <gxuliang@f973dc9e-4b64-11de-b25e-a75f7371a613>
2009-09-22 17:11:14 +0000

    增加了9270处理模块，电话来电检测模块，eeprom读写模块。

--- 报警器/code/alarm/src/eeprom.c
+++ 报警器/code/alarm/src/eeprom.c
@@ -4,44 +4,46 @@
 **         (c) Copyright 2009-2010, XXXX CO.LTD.
 **                                    All Rights Reserved
 **  ==========================================================================================
-**	File		: phone.h
+**	File		: eeprom.c
 **  ==========================================================================================
 **	Description	: Application enter pointer
 **  ==========================================================================================
 **  History     : Modify by  ||    ID    ||     Date      ||     Contents
-**              :   xul      ||          ||   2009/09/14  || Create this file
+**              :   xul      ||          ||   2009/09/22  || Create this file
 **  ******************************************************************************************
 */
-
-#ifndef _PHONE_H_
-#define _PHONE_H_
 #include "def.h"
+#define _EEPROM_C_
 
-#define DIAL_CLK    (1 << PC2)
-#define DIAL_DATA   (1 << PC3)
-#define DIAL_CS     (1 << PC0)
+void EEPROM_write(unsigned int uiAddress, unsigned char ucData)
+{
+/* 等待上一次写操作结束 */
+while(EECR & (1<<EEPE))
+;
+/* 设置地址和数据寄存器 */
+EEAR = uiAddress;
+EEDR = ucData;
+/* 置位EEMWE */
+EECR |= (1<<EEMPE);
+/* 置位EEWE 以启动写操作E */
+EECR |= (1<<EEPE);
+}
 
-#define CSC9270_DV_PIN  (1 << PC5)
-#define CSC9270_DV_DIR  DDRC
-#define CSC9270_DV_STA  (PORTC & CSC9270_DV_PIN)
 
-#define CSC9270_D0  (1 << PB3)
-#define CSC9270_D1  (1 << PB4)
-#define CSC9270_D2  (1 << PB5)
-#define CSC9270_D3  (1 << PC6)
-#define CSC9270_D2_D0_DIR DDRB
-#define CSC9270_D3_DIR DDRC
-#define CSC9270_D2_D0_STA   ((PORTB & (CSC9270_D2 | CSC9270_D1 | CSC9270_D0)) >> 3)
-#define CSC9270_D3_STA      (((PORTC & CSC9270_D3) >> 3))
-#define CSC9270_OE  (1 << PD7)
-#define CSC9270_OE_DIR  DDRD
-#define CSC9270_OE_CTL  PORTD
+unsigned char EEPROM_read(unsigned int uiAddress)
+{
+/* 等待上一次写操作结束 */
+while(EECR & (1<<EEPE))
+;
+/* 设置地址寄存器 */
+EEAR = uiAddress;
+/* 设置EERE 以启动读操作 */
+EECR |= (1<<EERE);
+/* 自数据寄存器返回数据 */
+return EEDR;
+}
 
-const char csc9270_dtmf_tab[] = {0x0a,0x01,0x02,0x03,0x04,0x05,0x06,0x07,0x08,\
-                                 0x09,'*','#','A','B','C','D'};
 
-#endif
 /*=============================================================================
 ==============================END OF THE FILE==================================
 ==============================================================================*/
-
--- 报警器/code/alarm/src/phone.c
+++ 报警器/code/alarm/src/phone.c
@@ -16,6 +16,8 @@
 #include "def.h"
 #include "phone.h"
 
+bool g_bDetRingFst = 0;//ring come flag
+
 static void clk_h(void)
 {
 	PORTC |= DIAL_CLK;
@@ -79,6 +81,79 @@ void dial_num(uint8 *p, uint8 num)
 		put_9200a(p[i]);
 	put_9200a(0xFF);	//stop code
 	disable_cs();
+}
+
+/*----------------------------------------------------------------------------
+---------------------------phone receive mode---------------------------------
+------------------------------------------------------------------------------*/
+
+uint8 dtmf_read(void)
+{
+  char val=0;
+  val = CSC9270_D2_D0_STA;
+  val |= CSC9270_D3_STA;
+  val &= 0x0f;
+  /*
+  if(val >= sizeof(csc9270_dtmf_tab))
+    return 0xff;
+  */
+  return csc9270_dtmf_tab[val];
+}
+
+uint8 dtmf_detect(void)
+{
+  uint8 val = 0xff;
+  
+  /*set D3-D0 input mode*/
+  CSC9270_D2_D0_DIR &= ~(CSC9270_D0 | CSC9270_D1 | CSC9270_D2);
+  CSC9270_D3_DIR &= ~CSC9270_D3;
+  
+  CSC9270_DV_DIR &= ~CSC9270_DV_PIN;//set input mode
+  CSC9270_OE_DIR |= CSC9270_OE;//set output mode
+  CSC9270_OE_CTL &= CSC9270_OE;//init the output low
+  if(CSC9270_DV_STA != 0)//have the dtmf signal
+  {
+     CSC9270_OE_CTL |= CSC9270_OE;//D0-D3 输出使能高电平有效
+     __delay_cycles(20);
+     val = dtmf_read();
+     CSC9270_OE_CTL &= CSC9270_OE;
+     return val;
+  }
+  return 0xff;
+}
+
+//**************************************
+//外部中断0,下降沿触发（参考电话模块部分）
+//
+//
+//*************************************
+void phone_init(void)
+{
+  __disable_interrupt();
+  PORTB |= (1 << PB0); 
+  DDRB &= ~(1 << DDB0);//捕获端口设置为输入
+  
+  EICRA = 0x02;//int0 下降沿触发
+  EIMSK = 0x01;//int0 enable
+  __enable_interrupt();
+}
+
+#pragma vector = INT0_vect
+__interrupt void into_for_phone_isr(void)
+{
+   static uint8 ucCount = 0x00;
+
+
+    ucCount = ucCount + 1;
+
+    if (2 == ucCount)      //ring is ringing   three times
+    {
+        g_bDetRingFst = 1;  //have phone  
+        ucCount = 0;
+
+    }
+
+
 }
 /*=============================================================================
 ==============================END OF THE FILE==================================
