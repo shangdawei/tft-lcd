7532dd1cf518020149d5220e41d157ffb435ac2d
gxuliang <gxuliang@f973dc9e-4b64-11de-b25e-a75f7371a613>
2009-09-22 17:11:14 +0000

    增加了9270处理模块，电话来电检测模块，eeprom读写模块。

--- trunk/报警器/code/alarm/inc/eeprom.h
+++ trunk/报警器/code/alarm/inc/eeprom.h
@@ -4,41 +4,27 @@
 **         (c) Copyright 2009-2010, XXXX CO.LTD.
 **                                    All Rights Reserved
 **  ==========================================================================================
-**	File		: phone.h
+**	File		: eeprom.h
 **  ==========================================================================================
 **	Description	: Application enter pointer
 **  ==========================================================================================
 **  History     : Modify by  ||    ID    ||     Date      ||     Contents
-**              :   xul      ||          ||   2009/09/14  || Create this file
+**              :   xul      ||          ||   2009/09/22  || Create this file
 **  ******************************************************************************************
 */
 
-#ifndef _PHONE_H_
-#define _PHONE_H_
-#include "def.h"
+#ifndef _EEPROM_H_
+#define _EEPROM_H_
 
-#define DIAL_CLK    (1 << PC2)
-#define DIAL_DATA   (1 << PC3)
-#define DIAL_CS     (1 << PC0)
+#ifndef _EEPROM_C_
+#define GLOGLE_EEPROM   extern
+#else
+#define GLOGLE_EEPROM
+#endif
 
-#define CSC9270_DV_PIN  (1 << PC5)
-#define CSC9270_DV_DIR  DDRC
-#define CSC9270_DV_STA  (PORTC & CSC9270_DV_PIN)
+GLOGLE_EEPROM void EEPROM_write(unsigned int uiAddress, unsigned char ucData);
+GLOGLE_EEPROM unsigned char EEPROM_read(unsigned int uiAddress);
 
-#define CSC9270_D0  (1 << PB3)
-#define CSC9270_D1  (1 << PB4)
-#define CSC9270_D2  (1 << PB5)
-#define CSC9270_D3  (1 << PC6)
-#define CSC9270_D2_D0_DIR DDRB
-#define CSC9270_D3_DIR DDRC
-#define CSC9270_D2_D0_STA   ((PORTB & (CSC9270_D2 | CSC9270_D1 | CSC9270_D0)) >> 3)
-#define CSC9270_D3_STA      (((PORTC & CSC9270_D3) >> 3))
-#define CSC9270_OE  (1 << PD7)
-#define CSC9270_OE_DIR  DDRD
-#define CSC9270_OE_CTL  PORTD
-
-const char csc9270_dtmf_tab[] = {0x0a,0x01,0x02,0x03,0x04,0x05,0x06,0x07,0x08,\
-                                 0x09,'*','#','A','B','C','D'};
 
 #endif
 /*=============================================================================
