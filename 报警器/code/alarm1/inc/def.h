645419d6779f540c3a9118d90084d61f01ae499b
gxuliang <gxuliang@f973dc9e-4b64-11de-b25e-a75f7371a613>
2010-08-23 11:16:11 +0000

    添加报警器版本管理！！

---
+++ 报警器/code/alarm1/inc/def.h
@@ -0,0 +1,43 @@
+/*
+**  ******************************************************************************************
+**                                      ALARM
+**         (c) Copyright 2009-2010, XXXX CO.LTD.
+**                                    All Rights Reserved
+**  ==========================================================================================
+**	File		: def.h
+**  ==========================================================================================
+**	Description	: Application enter pointer
+**  ==========================================================================================
+**  History     : Modify by  ||    ID    ||     Date      ||     Contents
+**              :   xul      ||          ||   2009/09/14  || Create this file
+**  ******************************************************************************************
+*/
+#ifndef   _DEF_H_
+#define   _DEF_H_
+// Include I/O definition file
+#define  ENABLE_BIT_DEFINITIONS //don't del this line
+//#include <ioavr.h>
+#include <intrinsics.h>
+//! type redefinition 
+typedef unsigned char           uint8;
+typedef unsigned short          uint16;
+typedef signed long              int32;
+typedef unsigned long            uint32;
+
+typedef signed char             int8;
+
+//typedef unsigned char              bool;
+typedef unsigned long long      UQWORD;
+typedef long long               SQWORD;
+
+#define START   1
+#define STOP    0
+
+#ifndef NULL
+#define NULL 0
+#endif
+
+#endif
+/*=============================================================================
+==============================END OF THE FILE==================================
+==============================================================================*/
---
+++ 报警器/code/alarm1/main.c
@@ -0,0 +1,10 @@
+#include "def.h"
+
+int a = 0
+
+int main( void )
+{
+  
+  fdsvvv
+  return 0;
+}
