b8fb45fef4465d3cdb7121fcd24ab2650d40432d
gxuliang <gxuliang@f973dc9e-4b64-11de-b25e-a75f7371a613>
2010-08-25 15:55:53 +0000

    添加KeyRead函数实现代码

--- 报警器/code/alarm1/src/key.c
+++ 报警器/code/alarm1/src/key.c
@@ -21,6 +21,23 @@
 
 //用来缓冲键值
 static BYTE lsbuf[32]="";
+const BYTE KeyVal[]={1, 2, 3, 4, 5, 6, 7, 8, 9, 0, 0xA, 0xB, 0xC, 0xD, 0xE, 0xF};
+//////////////////////////////////////////////////////////////////////////
+///
+///     判断是否处于编程模式
+///     @return bool值，1-是0-不是
+///     @author     xuliang<gxuliang@gmail.com>
+///     @date       2010-08-25
+//////////////////////////////////////////////////////////////////////////
+static BYTE IsProgaMode(void)
+{
+   GPIOE->DDR &= ~0x01;//PE0 as 24LC04B_WP
+   GPIOE->CR1 |= 0x01;//PE0 input with pull-up
+   if(GPIOE->IDR & 0x01)
+     return 1;//编程模式
+   else 
+     return 0;
+}
 //////////////////////////////////////////////////////////////////////////
 ///
 ///     轮询，读取键值，需要注意的是要区别在编程模式和非编程模式下轮询的按键数不一样
@@ -31,7 +48,51 @@ static BYTE lsbuf[32]="";
 //////////////////////////////////////////////////////////////////////////
 static BYTE KeyRead(BYTE* ch)
 {
+  BYTE i;
+  if(IsProgaMode() == 0)
+    return 0;
   
+  //先把状态清掉
+  GPIOD->ODR &= ~GPIO_PIN_2;//PD2 as AB
+  for(i = 0; i < 8; i++)
+  {
+    //PC3产生一个上升沿，可能要加延迟
+    GPIOC->ODR &= ~GPIO_PIN_3;//PC3 as CLK
+    GPIOC->ODR |= GPIO_PIN_3;//PC3 as CLK
+  }
+  
+  GPIOD->CR1 &= ~0x18;//8P 9P 下拉
+  GPIOD->ODR |= GPIO_PIN_2;
+  GPIOC->ODR &= ~GPIO_PIN_3;//PC3 as CLK
+  GPIOC->ODR |= GPIO_PIN_3;//PC3 as CLK
+  if(GPIOD->IDR & 0x10)
+  {
+    *ch = KeyVal[0];
+    return 1;
+  }
+  if(GPIOD->IDR & 0x08)
+  {
+    *ch = KeyVal[8];
+    return 1;
+  }
+  
+  GPIOD->ODR &= ~GPIO_PIN_2;//PD2 as AB
+  for(i = 1; i < 8; i++)
+  {
+    GPIOC->ODR &= ~GPIO_PIN_3;//PC3 as CLK
+    GPIOC->ODR |= GPIO_PIN_3;//PC3 as CLK
+    if(GPIOD->IDR & 0x10)
+    {
+      *ch = KeyVal[i];
+      return 1;
+    }
+    if(GPIOD->IDR & 0x08)
+    {
+      *ch = KeyVal[i+8];
+      return 1;
+    }
+  }
+  return 0;
 }
 //////////////////////////////////////////////////////////////////////////
 ///
@@ -85,5 +146,5 @@ void KeyProcess(void)
 //////////////////////////////////////////////////////////////////////////
 void KeyInit(void)
 {
-  
+  GPIOD->DDR &= ~0x18;//PD4 as 8P, PD3 as 9P
 }
